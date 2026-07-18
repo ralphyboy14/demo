6a71db0509fc4362e91c72522d00c4bebc4e3136
ralphyboy22 <lightandheavyrepairshop@gmail.com>
2026-07-18 09:26:03 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+$simpleArray = [1,2,3,4,5];
+$associativeArray = ['name' => 'John',
+                    'age' => 30,
+                    'city' => 'New York'];
+
+
+                // echo $simpleArray[0];
+                // echo $associativeArray['name'];
+
+                $matrix = [
+
+                    [1,2,3],
+                    [4,5,6],
+                    
+                ];
+
+
+                // echo $matrix[1][0];
+
+                $fruits = ['banana' , 'apple', 'orange'];
+                var_dump(count($fruits));
+                sort($fruits);
+                var_dump($fruits);
+                rsort($fruits);
+                var_dump($fruits);
+
+            var_dump($associativeArray);
+            asort($associativeArray);
+            var_dump($associativeArray);
+            ksort($associativeArray);
+            var_dump($associativeArray);
+
+
+            $numbers = range (1,5);
+            var_dump($numbers);
+
+            $squared = array_map(fn($n) => $n ** 2 , $numbers);
+            var_dump($squared);
+            // $evenNumbers = array_filter($numbers)
+
+            $evenNumbers = array_filter($numbers, fn($n) => $n % 2 == 0);
+            var_dump($evenNumbers);
+
+            $sum = array_reduce($numbers, fn($carry , $n ) => $carry + $n, 0);
+            var_dump($sum);
+
+            $moreNumbers = [0 , ...$numbers, 6 ];
+            var_dump($moreNumbers);
+
+            
+
+?>
